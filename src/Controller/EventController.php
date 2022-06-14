86919439a28462b1fbb6b20e2dbfcd84373a7d6b
ipogartsev <pogartseva.irina@gmail.com>
2022-06-14 11:06:12 +0200

    ajout fonction affichages sur eventController

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -5,25 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EventRepository;
 
 class EventController extends AbstractController
 {
     /**
      * @Route("/events", name="app_events")
      */
-    public function index(): Response
+    // Affichage des evenements sur la page     
+    public function index(EventRepository $eventRepository): Response
     {
+      // Chercher les evenements dans BDD
+      $events = $eventRepository->findAll();
+
+      // Retrouver la date d'aujourd'hui
+      $today = date("Y-m-d H:i:s");
+      // Parcourir 'events' pour selectionner les evenements en date d'aujourd'hui
+        foreach($events as $event){
+          $eventDate = $event->getDateOfEvent();
+          if($today < $eventDate){
+            $futureEvents[] = $event;         
+          }
+        }
+
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'futureEvents' => $futureEvents,
         ]);
     }
+
     /**
-     * @Route("events/activite", name="app_events/activite")
+     * @Route("events/{title}", name="app_event_details")
      */
-    public function activite(): Response
-    {
-        return $this->render('event/activite.html.twig', [
-            'controller_name' => 'EventController',
-        ]);
+    // page d'ffichage d'une activité
+    public function eventDetails(EventRepository $eventRepository, string $title): Response
+    { 
+      //Recuperer les details d'evenement
+      $event = $eventRepository ->findOneBy(['title' => $title]); 
+      //  
+      return $this->render('personality/activite.html.twig', [ 'event' => $event ]); 
     }
+    
 }
