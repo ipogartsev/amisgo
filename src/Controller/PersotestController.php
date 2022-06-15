fad9e7661359a3208c3eef269a094ee302182fe4
Angélina RIET <angelina.riet@yahoo.com>
2022-06-15 10:34:51 +0200

    fonction pour trouver la catégorie

--- src/Controller/PersotestController.php
+++ src/Controller/PersotestController.php
@@ -7,22 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\QuestionRepository;
-use App\Repository\AnswersRepository;
-use PhpParser\Node\Stmt\Foreach_;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
 
 class PersotestController extends AbstractController
 {
     /**
      * @Route("/persotest", name="app_persotest")
      */
-    public function index(QuestionRepository $questionRepository, AnswersRepository $answersRepository): Response
+    public function index(QuestionRepository $questionRepository): Response
     {
-        //permet d'afficher la première série de questions
+        //permet d'afficher la première série de questions. Il s'agit de la série 0.
         $firstSetOfQuestions = $questionRepository->findBy(['question_set' => 0]);
-        // dd($firstSetOfQuestions);
-
-        //    $secondSetOfQuestions = $questionRepository->findBy(['question_set' => $question_set]);
 
         return $this->render(
             'persotest/index.html.twig',
@@ -33,20 +29,48 @@ class PersotestController extends AbstractController
     /**
      * @Route("/persotest/page2", name="app_persotest2")
      */
-    public function getSecondSetOfQuestion(Request $request, QuestionRepository $questionRepository)
+    public function getSecondSetOfQuestions(Request $request, QuestionRepository $questionRepository)
     {
-        //dd($request);
+        // dd($request);
+        //boucle pour récupérer la catégorie des réponses de la première série de questions dans un "array"
         $tab = [];
         $firstSetOfQuestions = $questionRepository->findBy(['question_set' => 0]);
         foreach ($firstSetOfQuestions as $row) {
-            $param = $request->get('flexRadioDefault_' . $row->getId());
-            array_push($tab, $param);
+            //on récupére la valeur du "name" dans le formulaire page 1 "flexRadioDefault" avec l'id concaténné des réponses selectionnées
+            $param = $request->get('flexRadioDefault' . $row->getId());
+            //on insère l'ensemble des "id_category" des réponses selectionnées dans le tableau $tab
+            // l'id_category correspond aux catégories de personnalités (il y en a 4 qui contiennent chacune 3 personnalités)
+            if (!isset($tab[$param])) {
+                $tab[$param] = 1;
+            } else {
+                $tab[$param] = $tab[$param] + 1;
+            }
         }
-        dd($tab);
+        // $tab2 =[];
+        // $idCategory =
 
-        // dd($listOfAnswersByCategory);
-        // $request->getIdCategory();
-        // $result = [];
-        // $responsesFirstSet = [];
+        // foreach ($tab as $idCategory => $row){
+        //     $tab2[$row]=$tab2[]+1;
+        // }
+        //trie le tableau en ordre décroissant
+        arsort($tab);
+        // dd($tab);
+        $equaltab = [];
+        $max = 0;
+        foreach ($tab as $key => $value) {
+            if ($max == 0) {
+                $max = $value;
+            }
+            if ($value == $max) {
+                $equaltab[] = $key;
+            }
+        }
+        // //dd($equaltab);
+        // $cat=random_int((count($equaltab),$equaltab.len)-1);
+        // dd($cat);
+        // // if(count($equaltab)!=1){
+
+        // // }
+
     }
 }
