5e43aa99f50a8c6b67beb5982ec95e6e721c553e
Irina POGARTSEV <pogartseva.irina@gmail.com>
2022-06-24 20:21:36 +0200

    Update filtre.js

--- public/javascript/filtre.js
+++ public/javascript/filtre.js
@@ -1,33 +1,31 @@
+
+// Création de tableau de catégory
+let filtre = {'sport': 'Sport', 'pleinair' : 'Plein air', 'musique': 'Musique', 'cuisine' : 'Cuisine', 'jeux' : 'Jeux', 'culture' : 'Culture', 'bienetre': 'Bien-être', 'apero':'Apéro'};
+
 // Selectionner les butons du filtre
 let btn = document.querySelectorAll(".filter-button");
 
-//let article = document.querySelector('article');
-
+// Ajouter ecouteur de clique pour chaque buton de filtre
 btn.forEach(element => {
   element.addEventListener('click', displayChoice);
 });
 
 function displayChoice(e){
-  /*if(article.dataset.category!==e.currentTarget.id){
-    article.style.value = "display:none";
-  }*/
-   console.log(e.currentTarget.id); 
+  // création variable qui contient la categorie definie par le buton clique
+  let target = filtre[e.currentTarget.id];
 
-   let allElements = document.querySelectorAll('[data-category]');
-   
-    let filtre = {'sport': 'Sport', 'pleinair' : 'Plein air', 'musique': 'Musique', 'danse' : 'Danse', 'cuisine' : 'Cuisine', 'jeux' : 'Jeux', 'spectacle' : 'Spectacle', 'culture' : 'Culture', 'bienetre': 'Bien-être', 'apero':'Apéro'};
-  let target = "";
-   console.log(allElements);
-   allElements.forEach(element => {
-    console.log(element.dataset.category);
-    target = filtre[e.currentTarget.id];
-    if(element.dataset.category!==target){
+  // Recuperer les categories de tous evenements de page
+  let allElements = document.querySelectorAll('[data-category]');
+  
+  // Parcourir le tableau pour selectioner les evenements à afficher qui ont la meme categorie que target
+  allElements.forEach(element => {
+    // Si la categorie d'evenement ne correspond pas à target - cacher l'evenement
+    if(element.dataset.category !== target){
       element.style = "display:none !important";
-    
-   }else{
-    element.style ="display:block";
-   }
-
+    // Si non afficher evenement
+    }else{
+      element.style ="display:block";
+    }
   })
 }
 
