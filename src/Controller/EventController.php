6874642109513ebba21c1df3cfdf7954a35674c9
Irina POGARTSEV <pogartseva.irina@gmail.com>
2022-06-24 12:21:09 +0200

    debug affichage

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -27,7 +27,7 @@ class EventController extends AbstractController
     public function index(EventRepository $eventRepository, UserRepository $userRepository): Response
     {
       //Création d'un tableau regroupant les checkbox 
-      $filtre = ['sport'=>'Sport', 'pleinair' => 'Plein air', 'musique' => 'Musique', 'danse' => 'Danse', 'cuisine' => 'Cuisine', 'jeux' => 'Jeux', 'spectacle' => 'Spectacle', 'culture' => 'Culture', 'bienetre'=> 'Bien-être', 'apero'=>'Apéro'];
+      $filtre = ['sport'=>'Sport', 'pleinair' => 'Plein air', 'musique' => 'Musique', 'cuisine' => 'Cuisine', 'jeux' => 'Jeux', 'culture' => 'Culture', 'bienetre'=> 'Bien-être', 'apero'=>'Apéro'];
       
       // Message  à afficher en cas d'absence des evenements
       $message = '';
